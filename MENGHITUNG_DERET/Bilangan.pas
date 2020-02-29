3ba6643f36673afd57b041917730e641883b043d
Cecep Maulana <50282224+mcecep456@users.noreply.github.com>
2020-02-29 13:57:44 +0700

    Add files via upload

---
+++ MENGHITUNG_DERET/Bilangan.pas
@@ -0,0 +1,20 @@
+program Bilangan;
+uses crt;
+var
+  bila, bilb, bilsem, deret, i: integer;
+begin
+   write('Masukkan Jumlah Deret   : '); readln(deret);
+   bila    := 0;
+   bilb    :=1;
+   bilsem  := 0;
+   for i := 1 to deret do
+   begin
+   bilsem := bila + bilb;
+   bila   := bilb;
+   bilb  := bilsem;
+   write(' ', bila,' ');
+  end;
+  Readkey;
+  write('Setiap Kesulitan Pasti Ada Kemudahan Yang Di Berikan Oleh ALLAH SWT!!!');
+  readln;
+end.
